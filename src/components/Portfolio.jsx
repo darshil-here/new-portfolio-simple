29468bb81aa94b06b987aadeea1b64b5ee2d2d7a
darshil-here <darshil.0406@gmail.com>
2024-09-22 23:04:56 +0530

    commit3

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -6,7 +6,7 @@ const Portfolio = () => {
       <div className="text-white text-base font-thin flex flex-col gap-3 text-center sm:text-left">
         <a
           target="_blank"
-          href="https://color-change-react-proj.netlify.app/"
+          href="https://currency-converter-react-proj.netlify.app/"
           className="flex items-center gap-2 hover:underline underline-offset-2 font-light justify-center sm:justify-start"
         >
           <svg
@@ -21,8 +21,9 @@ const Portfolio = () => {
               fill="currentColor"
             ></path>
           </svg>
-          color changer
+          currency converter
         </a>
+
         <a
           target="_blank"
           href="https://random-password-gen-react.netlify.app/"
@@ -41,6 +42,25 @@ const Portfolio = () => {
             ></path>
           </svg>
           random password generator
+        </a>
+        <a
+          target="_blank"
+          href="https://color-change-react-proj.netlify.app/"
+          className="flex items-center gap-2 hover:underline underline-offset-2 font-light justify-center sm:justify-start"
+        >
+          <svg
+            width="12"
+            height="12"
+            viewBox="0 0 12 12"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M2.07102 11.3494L0.963068 10.2415L9.2017 1.98864H2.83807L2.85227 0.454545H11.8438V9.46023H10.2955L10.3097 3.09659L2.07102 11.3494Z"
+              fill="currentColor"
+            ></path>
+          </svg>
+          color changer
         </a>
       </div>
     </div>
